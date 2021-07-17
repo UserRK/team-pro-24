355eed874814f5dbf0d2808e301e66f2f8cd24de
UserRK <82591652+UserRK@users.noreply.github.com>
2021-07-17 12:46:31 +0000

    Deploying to gh-pages from @ UserRK/team-pro-24@2bb0702e9c15ff5be2e486a3f40aa3eb20a68042 🚀

--- src.b8cd23a9.js
+++ src.b8cd23a9.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-pro-24/src.789a01cd.js.map
+//# sourceMappingURL=/team-pro-24/src.b8cd23a9.js.map
