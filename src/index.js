957d25e67e4cb1f26df081b41d676f372deeef51
Roman Klimenko <romanklimenko.4acounts@gmail.com>
2021-07-21 12:37:25 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './sass/main.scss';
+
 // burger-menu-------------------------------------------------------------/
 
 (() => {
@@ -66,14 +67,32 @@ import './sass/main.scss';
   }
 })();
 
+// slide-------------------------------------------------------------------------/
 
-// masonry -----------------------------------------------------------------/
-
-$('.grid').masonry({
-  itemSelector: '.grid-item',
-  columnWidth: 160
+$(function () {
+    $('.rev_slider').slick({
+        arrows:false,
+		dots:true,
+		slidesToShow:1,
+		autoplay:false,
+		speed:1000,
+		autoplaySpeed:800,
+		responsive:[
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow:1
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow:1  
+				}
+			}
+		]
+	});
 });
-
 
 
 
